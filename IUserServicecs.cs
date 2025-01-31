70b9e5bee62f82229bc501d224912ab15a6405b5
zeyad mohamed <167231237+zoz14@users.noreply.github.com>
2025-01-31 20:06:52 +0200

    Add login

---
+++ IUserServicecs.cs
@@ -0,0 +1,16 @@
+﻿using StartUP.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartUP.Service.UserService
+{
+    public interface IUserServicecs
+    {
+        Task<bool> RegisterUserAsync(UserDto userDto);
+       Task<bool> LoginUserAsync(LoginDto loginDto);
+
+    }
+}
---
+++ UsersController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StartUP.Service.UserService;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IUserServicecs _userService;
+
+    public UsersController(IUserServicecs userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("SignUp")]
+    public async Task<IActionResult> SignUp([FromBody] UserDto userDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        if (userDto.Password != userDto.ConfirmPassword)
+        {
+            return BadRequest("Passwords do not match.");
+        }
+
+        var result = await _userService.RegisterUserAsync(userDto);
+        if (!result)
+        {
+            return Conflict("User with the same email already exists.");
+        }
+
+        return Ok("User registered successfully.");
+    }
+
+
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> LogIn(LoginDto login)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+
+        }
+
+        var result = await _userService.LoginUserAsync(login);
+        if (!result)
+        {
+            return Conflict("Oops! 😕 Incorrect email or password. Double-check your details and try again!");
+        }
+        return Ok("Welcome back! 🎉 You’ve successfully logged in. Let’s get started! 🚀");
+    }
+}
